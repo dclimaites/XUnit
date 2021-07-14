3df6f52ed85385ece2c302ce1418800ae264d891
Diego Climaites <diegoclimaites@gmail.com>
2021-07-14 20:25:29 -0300

    Adicionado método para validação de valor negativo no lance.

---
+++ Alura.LeilaoOnline.XUnitTestes/LanceCtor.cs
@@ -0,0 +1,22 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+using Xunit;
+
+namespace Alura.LeilaoOnline.XUnitTestes
+{
+    public class LanceCtor
+    {
+        [Fact]
+        public void LanceArgumentExceptionDadoLanceComValorNegativo()
+        {
+            //arrange
+            var leilao = new Leilao("Pablo Picasso");
+            var fulano = new Interessada("Fulano", leilao);
+            leilao.IniciaPregao();
+            //Assert
+            Assert.Throws<ArgumentException>(
+                //Act
+                () => leilao.RecebeLance(fulano, -100));
+        }
+    }
+}
